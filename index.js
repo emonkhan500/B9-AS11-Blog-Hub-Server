a6a81c3020e53240d6c266a04e8a2ed4874ab45e
emonkhan500 <khdilo47@gmail.com>
2025-05-19 20:44:45 +0600

    cors update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors({
   origin:[
    'http://localhost:5173',
-   'https://b9-assignment-11.web.app'
+   'https://assignment-blog-hub.netlify.app'
   ]
 }))
 app.use(express.json())
